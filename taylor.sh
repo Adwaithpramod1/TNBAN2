1b642ea44944cd7a9c8dea8f6d9bd181c2627c9e
TAYLOR <102683482+Adwaithpramod1@users.noreply.github.com>
2022-04-22 14:01:11 +0530

    Create taylor.sh

---
+++ taylor.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "L O A D I N G"|lolcat
+sleep 4
+clear
+
+cat logo.txt|lolcat
+
+echo "---------------------------------"
+echo "              MENU"
+echo "---------------------------------"
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[>
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[>
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[>
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[>
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Ent>
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+cd files
+bash NumberBan.sh
+elif [[ $option == 2 || $option == 02 ]]; then
+cd files
+bash AutoAdmin.sh
+elif [[ $option == 3 || $option == 03 ]]; then
+echo "TAYLOR IS WONDER FULL"|lolcat
+elif [[ $option == 4 || $option == 04 ]]; then       >
+exit 1
+else
+echo
+printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
+sleep 1
+echo
+fi
+exit
