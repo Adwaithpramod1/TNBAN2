97758a675d883144612d647c8d4fc55f683614ce
TAYLOR <102683482+Adwaithpramod1@users.noreply.github.com>
2022-04-23 08:58:56 +0530

    Update taylor.sh

--- taylor.sh
+++ taylor.sh
@@ -8,11 +8,11 @@ cat logo.txt|lolcat
 echo "---------------------------------"
 echo "              MENU"
 echo "---------------------------------"
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[>
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[>
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[>
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[>
-read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Ent>
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;36m AUTO ADMIN TOOL\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;36m NUMBER BANING TOOL\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;36m ABOUT TAYLOR\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;36m EXIt\e[0m\n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Enter Your Choice: \e[0m' option
 
 
 if [[ $option == 1 || $option == 01 ]]; then
@@ -23,7 +23,7 @@ cd files
 bash AutoAdmin.sh
 elif [[ $option == 3 || $option == 03 ]]; then
 echo "TAYLOR IS WONDER FULL"|lolcat
-elif [[ $option == 4 || $option == 04 ]]; then       >
+elif [[ $option == 4 || $option == 04 ]]; then
 exit 1
 else
 echo
